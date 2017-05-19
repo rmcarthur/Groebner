0530f44a6fd9ac6013504b9887ae98df3f67e2ac
zacharytaylor <zachtylr04@gmail.com>
2017-05-19 12:42:30 -0600

    vectorspace test

--- pytest/test_vectorspace.py
+++ pytest/test_vectorspace.py
@@ -19,4 +19,24 @@ def test_makeBasis():
     assert (len(basis) == len(trueBasis)) and (m in basis for m in trueBasis), \
             "Failed on MultiPower in 2 vars."
 
-    
+    f1_coeff = np.array([[[0,0,1],[0,3/20,0],[0,0,0]],[[0,0,0],[-3/40,1,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]]])
+    f2_coeff = np.array([[[3/16,-5/2,0],[0,3/16,0],[0,0,0]],[[0,0,1],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]]])
+    f3_coeff = np.array([[[0,1,1/2],[0,3/40,1],[0,0,0]],[[-1/2,20/3,0],[-3/80,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]]])
+    f4_coeff = np.array([[[3/32,-7/5,0,1],[-3/16,83/32,0,0],[0,0,0,0],[0,0,0,0]],[[3/40,-1,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]], \
+                        [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]])
+    f5_coeff = np.array([[[5,0,0],[0,0,0],[0,0,0]],[[0,-2,0],[0,0,0],[0,0,0]],[[1,0,0],[0,0,0],[0,0,0]]])
+    f6_coeff = np.array([[[0,0,0],[0,0,0],[1,0,0]],[[0,-8/3,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]]])
+
+    f1 = MultiPower(f1_coeff)
+    f2 = MultiPower(f2_coeff)
+    f3 = MultiPower(f3_coeff)
+    f4 = MultiPower(f4_coeff)
+    f5 = MultiPower(f5_coeff)
+    f6 = MultiPower(f6_coeff)
+
+    G = [f1, f2, f3, f4, f5, f6]
+    basis = vs.makeBasis(G)
+    trueBasis = [(0,0,0),(1,0,0),(0,1,0),(1,1,0),(0,0,1),(0,0,2),(1,0,1),(0,1,1)]
+
+    assert (len(basis) == len(trueBasis)) and (m in basis for m in trueBasis), \
+            "Failed on MultiPower in 3 vars."
